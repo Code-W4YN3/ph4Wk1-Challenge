9d2e9ee533c2c69f2b22d67fa6a73f85cd0c9445
wayne-otido <otidowayne@gmail.com>
2023-09-24 14:51:18 +0300

    Fixing Recursion error

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
-
+from models import db, Restaurant, Pizza, Restaurant_pizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza.db'
@@ -14,6 +15,71 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+@app.route('/')
+def index():
+    return '<h1>Restaurants API</h1> \n \n <h2>Endpoints: </h2> \n <p>All restaurants: /restaurants</p> \n <p>Restaurant by ID: /restaurants/[id]</p> \n <p>All pizzas: /pizzas</p> \n <p>Restaurant-pizzas: /restaurant_pizzas</p>'
+
+class Restaurants(Resource):
+
+    def get(self):
+        restaurants = [rest.to_dict() for rest in Restaurant.query.all()]
+        return make_response(jsonify(restaurants), 200)
+
+api.add_resource(Restaurants, '/restaurants')
+
+class RestaurantsID(Resource):
+
+    def get(self, id):
+        restaurantsID = [Restaurant.query.filter_by(id = id).first().to_dict()]
+        return make_response(jsonify(restaurantsID), 200)
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        restaurant_pizzas = Restaurant_pizza.query.filter_by(restaurant_id = id).all()
+
+        db.session.delete(restaurant_pizzas)
+        db.session.delete(restaurant)
+        db.session.commit()
+
+        response = make_response(
+            " ",
+            200
+        )
+
+        return response
+
+api.add_resource(RestaurantsID, '/restaurants/<int:id>')
+
+class Pizzas(Resource):
+
+    def get(self):
+        pizzas = [pizza.to_dict() for pizza in Pizza.query.all()]
+        return make_response(jsonify(pizzas), 200)
+    
+api.add_resource(Pizzas, '/pizzas')
+
+class Restaurant_pizzas(Resource):
+
+    def get(self):
+        rest_pizzas = [rest_pizza.to_dict() for rest_pizza in Restaurant_pizza.query.all()]
+        return make_response(jsonify(rest_pizzas), 200)
+    
+    def post(self):
+        
+        new_rest_pizza = Restaurant_pizza(
+            price = request.form['name'],
+            pizza_id = request.form['pizza_id'],
+            restaurant_id = request.form['restaurant_id'],
+        )
+
+        db.session.add(new_rest_pizza)
+        db.session.commit()
+
+        return make_response(new_rest_pizza.to_dict(), 201)
+
+api.add_resource(Restaurant_pizzas, '/restaurant_pizzas')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,63 @@
+#!/usr/bin/env python3
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    address = db.Column(db.String)
+
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas')
+
+    @validates('name')
+    def validates_name(self, key, name):
+        if(len(name) >= 50):
+            raise ValueError("Restaurant name cannot exceed 50 characters")
+        return name
+    
+    def __repr__(self):
+        return f'<Restaurant {self.name} | Address: {self.address}>'
+    
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurants= db.relationship('Restaurant', secondary='restaurant_pizzas')
+
+    def __repr__(self):
+        return f'<Pizza {self.name} | Ingredients: {self.ingredients}>'
+    
+
+class Restaurant_pizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pizza_id =  db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id =  db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    pizza = db.relationship(Pizza, backref=db.backref("restaurant_pizzas", cascade="all, delete-orphan"))
+    restaurant = db.relationship(Restaurant, backref=db.backref("restaurant_pizzas", cascade="all, delete-orphan"))
+
+    @validates('price')
+    def validates_price(self, key, price):
+        if(price not in range(1,31)):
+            raise ValueError("Price can only range from 1 to 30")
+        return price
+
+    def __repr__(self):
+        return f'<Pizza {self.name} | Ingredients: {self.ingredients}>'
